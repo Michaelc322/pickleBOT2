deb4581bca94cffd66c7d8f3fd40861c3d83b421
Michaelc322 <michaelc32204@gmail.com>
2024-02-21 11:19:25 -0500

    changes in response to test fail at drop down

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const cron = require('node-cron');
 const { url } = require('inspector');
 const fs = require('fs').promises;
+//const {TimeoutError} = require('puppeteer/Errors');
 
 
 const user = "mcarrolldev@gmail.com";
@@ -9,11 +10,11 @@ const password = "BotUserDev505#"
 
 // If Tom Brown CourtNum = 1-4
 // If Four Oaks CourtNum = 1-6
-const TimeSlot = 3;
-const CourtNum = 3;
+const TimeSlot = 8;
+const CourtNum = 4;
 
 // Tom Brown or Four Oaks
-const WhichCourt="Tom Brown"
+const WhichCourt="Four Oaks"
 
 
 
@@ -66,9 +67,9 @@ async function initBrowser(){
     // these lines of code r to test log in functionality
 
     // NEED THESE ON FIRST RUN
-
-    const cookies = await page.cookies();   
-    await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+    // const cookies = await page.cookies();   
+    // await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+    
     await loadCookie(page);
     await page.goto('https://app.courtreserve.com/Online/Account/Login/7975?isMobileLayout=False');
     await saveCookie(page);
@@ -113,25 +114,34 @@ async function reserve(page){
     const dateTimeObject = new Date();
     console.log(`Date: ${dateTimeObject.toDateString()}`);
     console.log(`Time: ${dateTimeObject.toTimeString()}`);
-    //await delay(10000);
 
     const dateTimeAfter = new Date();
     console.log(`Date: ${dateTimeAfter.toDateString()}`);
     console.log(`Time: ${dateTimeAfter.toTimeString()}`);
 
     // wait for here button
-    // await page.waitForSelector('#ReservationOpenTimeDispplay > span > a', {timeout: 5_000});
-    // click here
-    // await page.evaluate(()=> document.querySelector('#ReservationOpenTimeDispplay > span > a'))
+     //await page.waitForSelector('#ReservationOpenTimeDispplay > span > a', {timeout: 5_000});
+     try {
+        await page.waitForSelector('#ReservationOpenTimeDispplay > span > a', {timeout: 30_000});
+        await page.evaluate(()=> document.querySelector('#ReservationOpenTimeDispplay > span > a').click())
+
+    } catch (e) {
+            console.log('here button not displayed/clicked')
+          // Do something if this is a timeout.
+      }
+    
+     // click here
+     // await page.evaluate(()=> document.querySelector('#ReservationOpenTimeDispplay > span > a'))
 
 
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
-    await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // TESTING
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
+    // await page.evaluate(()=>document.querySelector('#CourtsScheduler > div > span.k-scheduler-navigation.k-button-group > button.k-button.k-button-md.k-rounded-md.k-button-solid.k-button-solid-base.k-icon-button.k-nav-next').click())
 
     await delay(2000);
     
@@ -144,17 +154,23 @@ async function reserve(page){
         console.log(TimeSlot, CourtNum)
         document.querySelector('#CourtsScheduler > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > table > tbody > tr:nth-child(' + TimeSlot + ') > td:nth-child(' + CourtNum + ') > span > button').click();
     }, TimeSlot, CourtNum);
+
+
     await page.waitForSelector('#reservation-general-info', {timeout: 30_000});
     // OPENS DROPDOWN FOR RESERVATION TYPE
     await page.evaluate(()=>document.querySelector('#reservation-general-info > div > div:nth-child(2) > div > div > span > button').click())
     // wait for drop down
+
     await page.waitForSelector('#ReservationTypeId-list > div.k-list-content.k-list-scroller', {timeout: 15_000});
-    
+    // NEW LINE to make sure doubles is loaded.
+    await page.waitForSelector('#ReservationTypeId_listbox > li:nth-child(2)', {timeout: 15_000});
+
     // select doubles
+    // first test failed here. click was null
     await page.evaluate(()=>document.querySelector('#ReservationTypeId_listbox > li:nth-child(2)').click())
 
     // wait for duration to update
-    await page.waitForSelector('#EndTime', {timeout: 5_000})
+    await page.waitForSelector('#EndTime', {timeout: 10_000})
 
     await page.evaluate(()=>document.querySelector('#Duration_listbox').click())
 
